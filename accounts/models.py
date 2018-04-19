67d0de291e5b75a3e95f21a727220a856206f9f9
apengok <apengok@163.com>
2018-04-19 17:34:30 +0800

    custome user

--- accounts/models.py
+++ accounts/models.py
@@ -1,25 +1,110 @@
-from django.conf import settings
+# -*- coding:utf-8 -*-
 from django.db import models
-from django.db.models.signals import post_save
-# Create your models here.
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+from django.utils.translation import gettext_lazy as _
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
-    phone = models.CharField(max_length=11,null=True,blank=True)
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
 
-    def __str__(self):
-        return str(self.user.username)
+        user = self.model(
+            email=self.normalize_email(email),
+        )
 
-    def __unicode__(self):
-        return str(self.user.username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, password):
+        """
+        Creates and saves a staff user with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
 
 
-def post_save_user_model_receiver(sender,instance,created,*args,**kwargs):
-    if created:
-        try:
-            Profile.objects.create(user=instance)
-        except:
-            pass
+class User(AbstractBaseUser):
 
-post_save.connect(post_save_user_model_receiver,sender=settings.AUTH_USER_MODEL)            
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    user_name   = models.CharField(_('user name'), max_length=30, blank=True)
+    real_name    = models.CharField(_('real name'), max_length=30, blank=True)
+    sex          = models.CharField(_('Sex'), max_length=30, blank=True)
+    phone_number = models.CharField(_('phone number'), max_length=30, blank=True)
+    belongto     = models.CharField(_('belongs to'), max_length=30, blank=True)
+    expire_date  = models.CharField(_('Expired date'), max_length=30, blank=True)
+    Role         = models.CharField(_('Role'), max_length=30, blank=True)
+
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False) # a admin user; non super-user
+    admin = models.BooleanField(default=False) # a superuser
+    # notice the absence of a "Password field", that's built in.
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] # Email & Password are required by default.
+
+    objects = UserManager()
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.staff
+
+    @property
+    def is_admin(self):
+        "Is the user a admin member?"
+        return self.admin
+
+    @property
+    def is_active(self):
+        "Is the user active?"
+        return self.active
